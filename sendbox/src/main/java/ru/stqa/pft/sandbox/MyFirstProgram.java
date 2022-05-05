fd5dfda8ed3ebb5f078eea7ffcc9f95d31abd687
Maria Rogovskaya <mtishinskikh@mail.ru>
2022-05-05 13:11:44 +0500

    Добавлено несколько функций

--- sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,27 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Maria");
+    
+    double l = 5;
+    System.out.println("Плащадь квадрата со стороной " +l + " = " + area(l));
 
-    System.out.println("Hello, world!");
+    double a = 4;
+    double b = 6;
+    System.out.println("Плащадь прямоугольника со сторонами " + a + " и " + b + area(a, b));
   }
 
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
